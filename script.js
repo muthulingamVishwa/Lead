775f658035180ec3d6f635c2dee8e317f974ac50
Vishwa Muthulingam <151023895+muthulingamVishwa@users.noreply.github.com>
2024-03-24 05:25:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function timestamp() { 
+    var response = document.getElementById("g-recaptcha-response"); 
+    if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+        elems["ts"] = JSON.stringify(new Date().getTime());
+        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
+     } } 
+     setInterval(timestamp, 500); 
+
+function capthcs(){
+document.querySelector('input[name="submit"]').classList.remove('hidden');
+
+}
+ function submitForm(event) {
+event.preventDefault(); 
+            
+            const firstName = document.getElementById('first_name').value;
+            localStorage.setItem('firstName', firstName); 
+            window.location.href = 'thank.html';
+ }
+
